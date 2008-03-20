8f4f70fa8f6d27cba4718e61ae6f282bae130048
thomasvl <thomasvl@c192c14b-6038-0410-9fcd-4bb9978d2467>
2008-03-20 03:30:56 +0000

    gcov files don't actually load.  the problem is that header w/ the type names has quoted cstrings, this is right for the code, but in the plist, the quotes don't belong, they result in quotes in the strings themselves.  fixed  w/ some nested CPP magic.
    git-svn-id: http://coverstory.googlecode.com/svn/trunk@30 c192c14b-6038-0410-9fcd-4bb9978d2467

--- CoverStoryDocumentTypes.h
+++ CoverStoryDocumentTypes.h
@@ -7,8 +7,20 @@
  *
  */
 
+
 // These are the various document types used by CoverStory.
 // Included in both Obj-C and plist sources.
-#define kGCNOTypeName "GNU Compiler Notes File"
-#define kGCDATypeName "GNU Compiler Data Arcs File"
-#define kGCOVTypeName "GNU Compiler Coverage File"
+
+// A little magic to get CPP to turn things into cstrings for the code but have
+// the strings raw for the plists.  The reason we need this is if we just put
+// them in quotes (cstrings) then we'd get those quotes in the plists, which we
+// don't want.
+#define STRINGIFY(x) #x
+#define TO_STRING(x) STRINGIFY(x)
+
+#define kGCNOTypeNameRaw GNU Compiler Notes File
+#define kGCDATypeNameRaw GNU Compiler Data Arcs File
+#define kGCOVTypeNameRaw GNU Compiler Coverage File
+#define kGCNOTypeName TO_STRING(kGCNOTypeNameRaw)
+#define kGCDATypeName TO_STRING(kGCDATypeNameRaw)
+#define kGCOVTypeName TO_STRING(kGCOVTypeNameRaw)
