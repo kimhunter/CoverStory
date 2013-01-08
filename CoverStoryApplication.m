12ada254a24517d42bd07c21df5a079b59ded589
Kim Hunter <bigkm1@gmail.com>
2013-01-08 23:12:37 +1000

    Formatting

--- CoverStoryApplication.m
+++ CoverStoryApplication.m
@@ -6,9 +6,9 @@
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not
 //  use this file except in compliance with the License.  You may obtain a copy
 //  of the License at
-// 
+//
 //  http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 //  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -24,49 +24,58 @@
 @end
 
 @implementation CoverStoryApplication
-- (id)handleOpenScriptCommand:(NSScriptCommand *)command {
-  NSArray *files = [command directParameter];
-  if ([files isMemberOfClass:[NSURL class]]) {
-    files = @[files];
-  }
-  NSDocumentController *docController 
-    = [NSDocumentController sharedDocumentController];
-  NSMutableArray *documents = [NSMutableArray arrayWithCapacity:[files count]];
-  NSError *error = nil;
-  for(NSURL *fileURL in files){
-    CoverStoryDocument *doc 
-      = [docController openDocumentWithContentsOfURL:fileURL
-                                             display:YES 
-                                               error:&error];
-    if (error) {
-      [command setScriptErrorNumber:(int)[error code]];
-      [command setScriptErrorString:[error localizedDescription]];
-      break;
+
+- (id)handleOpenScriptCommand:(NSScriptCommand *)command
+{
+    NSArray *files = [command directParameter];
+    if ([files isMemberOfClass:[NSURL class]])
+    {
+        files = @[files];
     }
-    [documents addObject:doc];
-  }
-  
-  // If we are opened via a script we want to wait until we are fully
-  // opened before returning. This prevents people from working on
-  // us in a half opened state.
-  NSRunLoop *loop = [NSRunLoop currentRunLoop];
-  BOOL stillOpening = YES;
-  while (stillOpening) {
-    stillOpening = NO;
-    for (CoverStoryDocument *doc in documents) {
-      if (![doc completelyOpened]) {
-        stillOpening = YES;
-        break;
-      }
+    NSDocumentController *docController = [NSDocumentController sharedDocumentController];
+    NSMutableArray *documents = [NSMutableArray arrayWithCapacity:[files count]];
+    NSError *error = nil;
+    for (NSURL *fileURL in files)
+    {
+        CoverStoryDocument *doc = [docController openDocumentWithContentsOfURL:fileURL
+                                                                       display:YES
+                                                                         error:&error];
+        if (error)
+        {
+            [command setScriptErrorNumber:(int)[error code]];
+            [command setScriptErrorString:[error localizedDescription]];
+            break;
+        }
+        [documents addObject:doc];
     }
-    if (stillOpening) {
-      [loop runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.5]];
+    
+    // If we are opened via a script we want to wait until we are fully
+    // opened before returning. This prevents people from working on
+    // us in a half opened state.
+    NSRunLoop *loop   = [NSRunLoop currentRunLoop];
+    BOOL stillOpening = YES;
+    while (stillOpening)
+    {
+        stillOpening = NO;
+        for (CoverStoryDocument *doc in documents)
+        {
+            if (![doc completelyOpened])
+            {
+                stillOpening = YES;
+                break;
+            }
+        }
+        if (stillOpening)
+        {
+            [loop runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.5]];
+        }
     }
-  }
-  
-  if ([documents count] == 1) {
-    documents = documents[0];
-  }
-  return documents;
+    
+    if ([documents count] == 1)
+    {
+        documents = documents[0];
+    }
+    return documents;
 }
+
 @end
